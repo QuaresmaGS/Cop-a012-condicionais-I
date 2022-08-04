ecb4df34584a11bf4dec73b9f579c3ac6671b7a4
Gabriel <gabrielssquaresma@gmail.com>
2022-08-04 15:40:33 -0300

    exercicios de fxa (condicionais)

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,22 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 20
+let isMaiorIdade = true
+let terminouOEnsinoMedio = true
+let isCursandoOutraFaculdade = true
 
-if(){
+if (idade >= 18) {
+    console.log(isMaiorIdade);
+}else if (idade <= 18){
+	console.log('ainda é menor de idade');
+};
 
-}else{
-	
-}
+if (terminouOEnsinoMedio === true){
+    console.log('Terminou o ensino médio');
+}else if(terminouOEnsinoMedio === false){
+    console.log('Não terminou o ensino médio.');
+};
+
+if (isCursandoOutraFaculdade === true){
+    console.log('Ésta fazendo facul');
+}else if(isCursandoOutraFaculdade === false){
+    console.log('Não terminou o ensino médio');
+};
